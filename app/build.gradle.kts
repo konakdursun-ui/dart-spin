4d901e535c762102719cd49d79a5aa464803201c
Dursun Konak <konakdursun-ui@users.noreply.github.com>
2026-07-02 09:14:01 +0300

    Bump Dart Spin version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.dkonak.dartat"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.8"
+        versionCode = 10
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
